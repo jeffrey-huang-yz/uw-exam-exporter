6a3f4d15945be37dd9bf340536199f5f07fb92fc
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-02-29 19:06:27 -0500

    Update service worker and manifest.json

--- frontend_react/src/content-script.ts
+++ frontend_react/src/content-script.ts
@@ -1,10 +1,11 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'CopyText') {
+        console.log('Message received in content script:', message);
         // Extract the text content of the webpage
         const textContent = document.body.innerText;
         // Send the text content back to the background script  
         sendResponse({ textContent });
-        
+
     }
 });
 export {};
