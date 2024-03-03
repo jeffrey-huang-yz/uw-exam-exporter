c71d982fba1b30e014e588cfbcd855bd03d92b0c
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-03-03 18:01:58 -0500

    Refactor serviceworker.js to use arrow functions and store activeTabId

--- frontend_react/serviceworker.js
+++ frontend_react/serviceworker.js
@@ -1,19 +1,25 @@
+var activeTabId;
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    activeTabId = activeInfo.tabId;
+    console.log('activeTabId', activeTabId);
+});
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.action === 'CopyText') {
-      console.log('message was received in background.js', message);
-      chrome.tabs.query({ active: true,currentWindow: true  }, function(tabs) {
-          console.log('tabs', tabs)
-          if (tabs[0]) {
-              console.log("seing message to content.js");
-              chrome.tabs.sendMessage(tabs[0].id, { action: 'CopyText',currentWindow: true }, function(response) {
-                  console.log("received response from content.js", response);
-                  sendResponse(response); // Sending content script response back
-              });
-          } else {
-              console.error('No active tab found');
-              sendResponse({ error: 'No active tab found' });
-          }
-      });
-      return true; // This indicates that sendResponse will be called asynchronously
-  }
+if (message.action === 'CopyText') {
+    console.log('message was received in background.js', message);
+    chrome.tabs.query({ active: true}, function(tabs) {
+    console.log('tabs', tabs)
+    if (tabs[0]) {
+        console.log("seing message to content.js");
+        chrome.tabs.sendMessage(activeTabId, { action: 'CopyText',currentWindow: true }, function(response) {
+            console.log("received response from content.js", response);
+            sendResponse(response); // Sending content script response back
+        });
+    } else {
+        console.error('No active tab found');
+        sendResponse({ error: 'No active tab found' });
+    }
+    });
+    return true; // This indicates that sendResponse will be called asynchronously
+}
 });
